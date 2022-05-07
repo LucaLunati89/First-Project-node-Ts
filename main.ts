d4236ef9ed73330e0554413dd9aba8260b2354c7
Luca <80696098+LucaLunati89@users.noreply.github.com>
2022-05-07 17:44:53 +0200

    first commit

---
+++ main.ts
@@ -0,0 +1,16 @@
+console.log("😃hello world!");
+let myVar : number = 13;
+console.log("my var = ", myVar );
+
+interface MyInterface {
+  myNumber : number,
+  myString : string
+}
+
+let myInterface : MyInterface = {
+  myNumber : 10,
+  myString : "ciao"
+}
+
+console.log("number = ", myInterface.myNumber);
+console.log("string = ", myInterface.myString);
